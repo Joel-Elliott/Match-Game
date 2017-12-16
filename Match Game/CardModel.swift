f0deb39c7760fcfde67b8c087c4cc88f4cd8591c
Joel Elliott <joelelliott819@gmail.com>
2017-12-16 17:54:51 -0600

    Assets & beginning of a class added - restricted to landscape

---
+++ Match Game/CardModel.swift
@@ -0,0 +1,28 @@
+//
+//  CardModel.swift
+//  Match Game
+//
+//  Created by Joel Elliott on 12/16/17.
+//  Copyright © 2017 Joel Elliott. All rights reserved.
+//
+
+import Foundation
+
+class CardModel {
+    
+    
+    func getCards() {
+        
+        //TODO: Declare an array to store generated cards
+        
+        //Randomly generate pairs of cards
+        
+        //randomize array
+        
+        //return array
+    }
+    
+    
+    
+    
+}
